d239c27dc549e2be635aa55666ff05593141f3c4
Tech-Brain01 <tomar.amrendera@outlook.com>
2025-08-26 16:12:45 +0530

    fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
-import { profile } from 'console'
 import { socialMedia } from '@/data'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
@@ -20,7 +20,7 @@ const Footer = () => {
                     {socialMedia.map((profile) => (
                         <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
                              <a href={profile.link} target="_blank" rel="noopener noreferrer">
-                                 <img src={profile.img} alt={profile.id.toString()} width="20" height="20" />
+                                 <Image src={profile.img} alt={profile.id.toString()} width={20} height={20} />
                              </a>
                         </div>
                     ))}
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { projects } from '@/data'
 import { PinContainer } from "./ui/3d-pin";
 import { FaLocationArrow } from "react-icons/fa";
+import Image from "next/image";
 
 const RecentProjects = () => {
   return (
@@ -18,9 +19,9 @@ const RecentProjects = () => {
                  <div className="relative  flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] 
                   mb-10">
                     <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                        <img src="/bg.png" alt="bg-img" />
+                        <Image src="/bg.png" alt="bg-img" layout="fill" objectFit="cover" />
                     </div>
-                      <img src={img} alt={title} className="z-10 absolute bottom-0"/>
+                      <Image src={img} alt={title} className="z-10 absolute bottom-0" width={570} height={300} />
                  </div>
                  <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clam-1">
                     {title}
