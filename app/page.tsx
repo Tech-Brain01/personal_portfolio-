8b1ceebec348c8e56dab0f86f683fcd523ea1232
Tech-Brain01 <tomar.amrendera@outlook.com>
2025-08-26 16:02:01 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 import { FaEnvelope, FaFolder, FaHome, FaUser } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip ">
       <div className="max-w-7xl w-full">
-          <FloatingNav navItems={[
-            { name: "Home", link: "#home", icon:<FaHome /> },
-            { name: "About", link: "#about", icon:<FaUser /> },
-            { name: "Projects", link: "#projects", icon:<FaFolder /> },
-            { name: "Contact", link: "#contact", icon:<FaEnvelope /> },
-          ]} />
+          <FloatingNav navItems={navItems} />
           <Hero />
           <Grid />
           <RecentProjects />
+          <Footer/>
        </div>
      </main>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -51,8 +51,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Express", "JavaScript"];
+  const rightLists = ["Docker", "Java", "DSA"];
 
   const [copied, setCopied] = useState(false);
 
